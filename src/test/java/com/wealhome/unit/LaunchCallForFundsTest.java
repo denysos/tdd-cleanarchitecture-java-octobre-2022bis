ebc2d3f24cbfa5f278b7314b860f4225d210a66c
Michaël AZERHAD <mazerhad@wealcomecompany.com>
2022-10-24 21:39:36 +0200

    create the template of the TDD suite

---
+++ src/test/java/com/wealhome/unit/LaunchCallForFundsTest.java
@@ -0,0 +1,11 @@
+package com.wealhome.unit;
+
+import org.junit.jupiter.api.Test;
+
+public class LaunchCallForFundsTest {
+
+    @Test
+    void AVousDeJouer_TrouvezCeQuIlFautSpecifierEnPremier() {
+
+    }
+}
